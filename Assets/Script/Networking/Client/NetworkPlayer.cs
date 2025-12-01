5e010e89d4ead362656269ed0ea6fe983af16af9
DevPotion <1k0j2i4@gmail.com>
2025-12-02 08:32:07 +0900

    "NetworkPlayer 클래스 추가"
    Signed-off-by: DevPotion <1k0j2i4@gmail.com>

---
+++ Assets/Script/Networking/Client/NetworkPlayer.cs
@@ -0,0 +1,11 @@
+using Mirror;
+
+namespace PixelCollector.Networking.Client
+{
+  public class NetworkPlayer
+  {
+    public NetworkConnectionToClient conn;
+    public string username;
+    public System.DateTime connectedAt;
+  }
+}
